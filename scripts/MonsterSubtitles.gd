b164be7c420ec4a8cbfd89252211f06cf27e661e
karolis dikcius <karolis.dikcius@gmail.com>
2023-02-05 10:41:27 +0200

    beginning to implement different subtitles

---
+++ scripts/MonsterSubtitles.gd
@@ -0,0 +1,11 @@
+extends RichTextLabel
+
+func _ready():
+    add_to_group("monster_subtitles")
+
+func show_subtitles(subtitles, timeout):
+    var line = "[center]" + subtitles + "[/center]"
+    self.set_bbcode(line)
+    $AnimationPlayer.play("fade_in")
+    yield(get_tree().create_timer(timeout), "timeout")
+    $AnimationPlayer.play("fade_out")
