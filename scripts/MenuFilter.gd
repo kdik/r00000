2d517b1bbfdfd1212ddf096aa3c1b0793a3d12c2
karolis dikcius <karolis.dikcius@gmail.com>
2023-05-21 10:00:00 +0300

    some more refactoring and updated readme

--- scripts/MenuFilter.gd
+++ scripts/MenuFilter.gd
@@ -4,14 +4,11 @@ func _ready():
     add_to_group("menu_filter")
 
 func start_playing():
-    if not is_playing(): play()
+    play()
     $AnimationPlayer.play("fade_in")
     yield(get_tree().create_timer($AnimationPlayer.get_animation("fade_in").length + 0.2), "timeout")
     
 func stop_playing():
     $AnimationPlayer.play("fade_out")
     yield(get_tree().create_timer($AnimationPlayer.get_animation("fade_out").length), "timeout")
-    if is_playing(): stop()
-
-func _on_finished():
-    play()
+    stop()
