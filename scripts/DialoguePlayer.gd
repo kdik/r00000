f7f1c4c0b7b62df2fdfc41bc34cf2ee2791826d2
karolis dikcius <karolis.dikcius@gmail.com>
2023-06-24 18:07:14 +0300

    quick fix for monster video glitch

--- scripts/DialoguePlayer.gd
+++ scripts/DialoguePlayer.gd
@@ -6,8 +6,11 @@ func _ready():
     visible = false
 
 func show_dialogue():
-    visible = true
     play()
+    yield(get_tree(), "idle_frame")
+    yield(get_tree(), "idle_frame")
+    yield(get_tree(), "idle_frame")
+    visible = true
     for _i in range(frame_count):
         yield(get_tree(), "idle_frame")
     visible = false
